c19fd620ec869741e7b91355057dd525b2478cd0
Divyesh <devanr2806@gmail.com>
2022-02-13 11:45:42 +0545

    Storing dataset for fetching carbon monoxide

--- src/resource/carbon_monoxide/core/fetch_carbon_monoxide_by_h3.py
+++ src/resource/carbon_monoxide/core/fetch_carbon_monoxide_by_h3.py
@@ -4,10 +4,14 @@ import pandas as pd
 
 
 def get_carbon_monoxide_details_by_h3(request_body):
-    start_day, end_day = get_date(interval=30)
     rolling = request_body.rolling
-    air_quality = fetch_carbon_monoxide_data(start_day, end_day)
-    h3_indexes = get_h3_indexes(air_quality)
+    air_quality = fetch_carbon_monoxide_data()
+    air_quality = settings.emission_data
+    if not settings.emission_data:
+        start_day, end_day = get_date(interval=30)
+        air_quality = fetch_carbon_monoxide_data(start_day, end_day)
+        settings.emission_data = air_quality.to_json()
+    h3_indexes = get_h3_indexes(gpd.GeoDataFrame.from_features(json.loads(air_quality))if type(air_quality) == str else air_quality)
     hdf = h3_indexes[h3_indexes['h3'] == request_body.h3_index]
     hdf_davg = hdf.groupby(pd.Grouper(freq='D', key='timestamp')).mean()
     hdf_davg.index= hdf_davg.index.date
